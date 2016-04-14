a175158876c9b4558cf3c5dc4c19ba02d0959621
gastontk <gastontk@hotmail.com>
2016-04-14 11:59:33 -0700

    added goodbye function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-  	render text: "<h1>Hello World! after change <h1/>"
+  	goodbye
+  	hello = "<h1>Hello World! after change <h1/> <h5> and the little version<h5/> "
+  end
+  def goodbye
+	render text: "<h3> #{hello}, And here is goodbye<h3/>"
+
   end
 end
